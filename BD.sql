6dfd560aef03000c25a1e8f42a614e5a06979339
IvanGuzman23 <70457183+IvanGuzman23@users.noreply.github.com>
2020-08-29 22:53:24 -0400

    Creacion de las tablas trabajadores y usuarios

--- BD.sql
+++ BD.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE IF NOT EXISTS `final_OYM` ;
 
 USE `final_OYM` ;
@@ -19,4 +18,33 @@ CREATE TABLE IF NOT EXISTS `final_OYM`.`Trabajos` (
   `min_salario` INT NULL,
   `max_salario` INT NULL,
   PRIMARY KEY (`idTrabajos`)
+);
+
+CREATE TABLE IF NOT EXISTS `final_OYM`.`Trabajadores` (
+  `idWorker` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `apellido` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `telefono` VARCHAR(45) NULL,
+  `salario` INT NULL,
+  `fk_idTrabajos` INT NOT NULL,
+  PRIMARY KEY (`idWorker`),
+  FOREIGN KEY (`fk_idTrabajos`)
+    REFERENCES `final_OYM`.`Trabajos` (`idTrabajos`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `final_OYM`.`usuarios` (
+  `idAdmin` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `apellido` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `telefono` VARCHAR(45) NULL,
+  `fk_idLogin` INT NOT NULL,
+  `fk_idWorker` INT NOT NULL,
+  PRIMARY KEY (`idAdmin`),
+  FOREIGN KEY (`fk_idLogin`)
+    REFERENCES `final_OYM`.`Login` (`idLogin`),
+  FOREIGN KEY (`fk_idWorker`)
+    REFERENCES `final_OYM`.`Trabajadores` (`idWorker`)
 );
